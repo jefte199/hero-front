2a502d31dd5be5f89181021d09d26e2ebb518557
Jefte <jefte199@gmail.com>
2021-06-26 19:54:45 -0300

    feat: create api.js and call api marvel

---
+++ src/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import md5 from 'js-md5';
+
+const timestamp = Number(new Date());
+
+const publick_key = "c04b58f6f79a46fca2174d79f512b1cc";
+//process.env.PUBLICK_KEY;
+
+const private_key = "a744cb2711a1facdddca11447190e8fe80f9c7d9";
+//process.env.PRIVATE_KEY;
+
+
+// create hash
+const hash_hero = md5.create();
+
+hash_hero.update(timestamp + private_key + publick_key);
+
+const api = axios.create({
+  baseURL: `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=10&apikey=${publick_key}&hash=${hash_hero.hex()}`
+})
+
+export default api;
